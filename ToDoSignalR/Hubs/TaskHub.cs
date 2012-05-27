a7ddcae80177243d9e92a7f92ea2be3bd3419d59
Paul Sainsbury <paul@digitaltinder.com>
2012-05-27 23:52:11 +0200

    Start of app
    I have 2 projects - one with a demo and some samples I was working
    through, and one that is becoming the main project.  Currently it simply
    keeps a simple track of some votes across all clients and lets clients
    reset the votes accordingly.

---
+++ ToDoSignalR/Hubs/TaskHub.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SignalR.Hubs;
+using ToDoSignalR.Models;
+
+namespace ToDoSignalR.Hubs
+{
+    public class TaskHub : Hub
+    {
+        public bool Add(Task newTask)
+        {
+            try
+            {
+                using (var context = new KsigDoContext())
+                {
+                    var task = context.Tasks.Create();
+                    task.title = newTask.title;
+                    task.completed = newTask.completed;
+                    task.lastUpdated = newTask.lastUpdated;
+
+                    context.Tasks.Add(task);
+                    context.SaveChanges();
+                    Clients.taskAdded(task);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                Caller.reportError("Unable to create task. Make sure title length is between 10 and 140");
+                return false;
+            }
+        }
+
+
+        public bool Update(Task updatedTask)
+        {
+            using (var context = new KsigDoContext())
+            {
+                var oldTask = context.Tasks.FirstOrDefault(t => t.taskId == updatedTask.taskId);
+                try
+                {
+                    if (oldTask == null)
+                        return false;
+                    else
+                    {
+                        oldTask.title = updatedTask.title;
+                        oldTask.completed = updatedTask.completed;
+                        oldTask.lastUpdated = DateTime.Now;
+                        context.SaveChanges();
+                        Clients.taskUpdated(oldTask);
+                        return true;
+                    }
+                }
+                catch (Exception)
+                {
+                    Caller.reportError("Unable to update task. Make sure title length is between 10 and 140");
+                    return false;
+                }
+            }
+        }
+
+
+        public bool Remove(int taskId)
+        {
+            try
+            {
+                using (var context = new KsigDoContext())
+                {
+                    var task = context.Tasks.FirstOrDefault(t => t.taskId == taskId);
+                    context.Tasks.Remove(task);
+                    context.SaveChanges();
+                    Clients.taskRemoved(task.taskId);
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Caller.reportError("Error : " + ex.Message);
+                return false;
+            }
+        }
+
+
+        public void GetAll()
+        {
+            using (var context = new KsigDoContext())
+            {
+                var res = context.Tasks.ToArray();
+                Caller.taskAll(res);
+            }
+
+        }
+    }
+}
---
+++ Voter/Hubs/Vote.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SignalR.Hubs;
+
+namespace Voter.Hubs
+{
+    public class Vote : Hub
+    {
+        public class Answer 
+        {
+            public string connectionid {get;set;}
+            public int vote {get;set;}
+
+        }
+        static List<Answer> votes = new List<Answer>();
+
+        public List<Answer> getVotes()
+        {
+            return votes;
+        }
+
+        public void resetVotes()
+        {
+            votes = new List<Answer>();
+            Clients.updateVotes("Votes reset");
+        }
+
+        public void setVote(int? vote)
+        {
+            string currentConnectionId = this.Context.ConnectionId;
+            Answer ans = votes.Find(a => a.connectionid == currentConnectionId);
+            if (ans == null)
+            {
+                ans = new Answer();
+                ans.connectionid = currentConnectionId;
+                votes.Add(ans);
+            }
+            ans.vote = vote.HasValue ? vote.Value : 0;
+            double average = 0;
+            if (votes.Count > 0)
+            {
+                foreach (Answer a in votes)
+                {
+                    average += a.vote;
+                }
+                average = average / votes.Count;
+            }
+            Clients.updateVotes(average + " over " + votes.Count + " votes");
+        }
+    }
+}
